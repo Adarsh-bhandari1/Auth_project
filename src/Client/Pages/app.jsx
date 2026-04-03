4953650ce34e5487ed5a7ba4d40bddb742986eed
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2026-04-03 20:48:17 +0530

    modified app.jsx for L1 Auth

--- src/Client/Pages/app.jsx
+++ src/Client/Pages/app.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import L1 from "./L1";
 function App() {
-  return <h1>Auth Project</h1>;
+  return (
+    <div>
+      <h1>Auth Project</h1>
+      <L1 />
+    </div>
+  )
 }
 
 export default App;
