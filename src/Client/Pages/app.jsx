cad4d479f9d53622e413cf078742c851eeed1f4b
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2026-04-06 23:34:55 +0530

    add some styling to l1 and title

--- src/Client/Pages/app.jsx
+++ src/Client/Pages/app.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import L1 from "./L1";
+import "../Css/style.css";
 function App() {
   return (
     <div>
-      <h1>Auth Project</h1>
-      <L1 />
+      <h1 className="h1_style">Auth Project</h1>
+      <div className="container">
+        <L1 />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
