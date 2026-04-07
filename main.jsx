e821c443da939269ab0f93f65deddd04d9bd0d78
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2026-04-07 17:41:14 +0530

    add css

--- main.jsx
+++ main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./src/Client/Pages/app.jsx";
 import { BrowserRouter } from "react-router-dom";
+import "./src/Client/Css/style.css";
 
 // ReactDOM.createRoot(document.getElementById("root")).render(
 //   <React.StrictMode>
