8ac8fbfe49464349ca535c2411d502cdf0a762e5
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2026-04-08 16:10:30 +0530

    add passport and create a post route for login

--- src/Server/server.js
+++ src/Server/server.js
@@ -1,9 +1,31 @@
 import express from 'express';
+import session from 'express-session';
+import passport from 'passport';
+import passportConfig from './auth.js';
+passportConfig();
 const app = express();
 const port = 3000;
+app.use(express.json());
+
+app.use(
+    session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge : 1000*60*60
+        },
+    }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+app.post("/login", passport.authenticate("local"), (req, res) => {
+  res.json({ msg: "Login Success", user: req.user });
+});
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
-app.get("/", (req, res) => {
-    res.send("Hello World");
+    console.log(`Server is running on port ${port}`);
 })
