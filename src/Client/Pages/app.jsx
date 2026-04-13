1c671132262beb1c29367090a206b65788b68922
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2026-04-13 22:32:09 +0530

    add button for l2

--- src/Client/Pages/app.jsx
+++ src/Client/Pages/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import L1 from "./L1";
+import L2 from "./L2";
 import "../Css/style.css";
 
 function Home() {
@@ -11,6 +12,7 @@ function Home() {
       <h1 className="h1_style">Auth Project</h1>
       <div className="container">
         <button onClick={() => navigate("/level1")}>Level 1</button>
+        <button onClick={() => navigate("/level2")}>Level 2</button>
       </div>
     </div>
   );
@@ -21,8 +23,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/level1" element={<L1 />} />
-      {/* <Route path="/Level2" element={<L2/>}/>    not implemented yet */}
-    </Routes> 
+      <Route path="/level2" element={<L2 />} />
+    </Routes>
   );
 }
 
