c909880850af497b415425e914ec3bf845a256a7
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2026-04-19 22:18:26 +0530

    Configure google oauth

--- src/Server/auth.js
+++ src/Server/auth.js
@@ -1,11 +1,21 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import dotenv from "dotenv";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, ".env") });
+
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 const user = {
     id : 1,
     username: "adarsh",
     password: "123"
 };
-
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = import.meta.env.VITE_GOOGLE_CLIENT_SECRET;
 function passportConfig() {
     passport.use(
         new LocalStrategy(
@@ -16,6 +26,18 @@ function passportConfig() {
             }
         )
     )
+    passport.use(
+      new GoogleStrategy(
+        {
+          clientID: GOOGLE_CLIENT_ID,
+          clientSecret: GOOGLE_CLIENT_SECRET,
+          callbackURL: "/auth/google/callback",
+        },
+        (accessToken, refreshToken, profile, done) => {
+          return done(null, profile);
+        },
+      ),
+    );
     passport.serializeUser((user, done) => {
         done(null, user.id);
     });
